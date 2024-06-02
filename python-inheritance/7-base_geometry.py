bd8f1b673e8454c83c8e7c20fbbdd3b7d4160114
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-06-02 16:40:17 +0000

    test

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,42 +1,18 @@
 #!/usr/bin/python3
-"""
-Module for a class Rectangle
-that inherits from BaseGeometry
-"""
+"""Module containing empty class"""
 
 
 class BaseGeometry:
-    """
-    Base geometry class
-    """
+    """Class for BaseGeometry"""
     def area(self):
-        raise Exception('area() is not implemented')
+        """Method to raise exception"""
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError('{} must be an integer'.format(name))
+        """Method to validate integer"""
+
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+
         if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
-
-class Rectangle(BaseGeometry):
-    """
-    Rectangle class that inherits from BaseGeometry
-    """
-    def __init__(self, width, height):
-        """
-        Init constructor
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Method that calculates the area of the rectangle
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)  
+            raise ValueError("{} must be greater than 0".format(name))
