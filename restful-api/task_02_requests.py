c03b0a68b215d7296d36a084dcc059bd552674a4
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-06-12 17:19:00 +0000

    Hotfix

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,33 +1,38 @@
-#!/usr/bin/python3
-
+#!/bin/python3
 import requests
 import csv
+
 
 def fetch_and_print_posts():
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
     print(f'Status Code: {response.status_code}')
-    
+
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
             print(post['title'])
+    else:
+        print("Failed to fetch posts")
+
 
 def fetch_and_save_posts():
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         posts = response.json()
-        formatted_posts = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
-        
+        posts_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+
         with open('posts.csv', 'w', newline='') as csvfile:
             fieldnames = ['id', 'title', 'body']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            
-            writer.writeheader()
-            writer.writerows(formatted_posts)
 
-if __name__ == "__main__":
+            writer.writeheader()
+            writer.writerows(posts_data)
+    else:
+        print("Failed to fetch posts")
+
+if __name__ == '__main__':
     fetch_and_print_posts()
     fetch_and_save_posts()
